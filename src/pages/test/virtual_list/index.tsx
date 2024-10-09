4d324a7537084e015db7f48632cf745eb6341499
blackroot <smallfish_root@126.com>
2024-10-09 08:41:10 +0800

    taro虚拟列表示例改造成函数组件

--- src/pages/test/virtual_list/index.tsx
+++ src/pages/test/virtual_list/index.tsx
@@ -1,13 +1,7 @@
-// const Index = () => {
-//   return <></>
-// };
-
 import { View } from "@tarojs/components"
 import { VirtualList } from "@tarojs/components-advanced/dist/components/virtual-list"
 import Taro from "@tarojs/taro"
-import React, { Component } from "react"
-
-// export default Index;
+import React, { useState } from "react"
 
 const Row = React.memo(({ id, index, data }:{id: string, index:number, data: any[]}) => {
   return (
@@ -23,67 +17,38 @@ function buildData(offset = 0) {
     .map((_, i) => i + offset)
 }
 
-export default class Index extends Component {
-  state = {
-    data: buildData(0),
-  }
+const Index = () => {
+  const [data, setData] = useState(buildData(0))
+  let loading = false
+  const listReachBottom = () => {
+    // 防止重复请求
+    if (loading) {
+      return
+    }
 
-  loading = false
-
-  listReachBottom() {
     Taro.showLoading()
-    // 如果 loading 与视图相关，那它就应该放在 `this.state` 里
-    // 我们这里使用的是一个同步的 API 调用 loading，所以不需要
-    this.loading = true
+    loading = true
     setTimeout(() => {
-      const { data } = this.state
-      this.setState(
-        {
-          data: data.concat(buildData(data.length)),
-        },
-        () => {
-          this.loading = false
-          Taro.hideLoading()
-        }
-      )
+      setData(data.concat(buildData(data.length)))
+      loading = false
+      Taro.hideLoading()
     }, 1000)
   }
 
-  render() {
-    const { data } = this.state
-    const dataLen = data.length
-    const itemSize = 100
-    // 获取元素高度
-    // let count;
-    //       Taro.createSelectorQuery().select('.List').boundingClientRect().exec((res) => {
-    //         console.log('----------------:', res[0].height)
-    //         count = res[0].height / itemSize
-    //       })
+  const dataLen = data.length
+  const itemSize = 100
     return (
       <VirtualList
         className='List'
-        height={500}
+        height={700} // 必须设置高度,不同的机型如何兼容
         item={Row}
         itemData={data}
         itemCount={dataLen}
         itemSize={itemSize}
         width='100%'
-        onScroll={({ scrollDirection, scrollOffset }) => {
-          console.log("+++++++++++:", this.loading, " ", scrollOffset, ' ', scrollDirection)
-          if (
-            // 避免重复加载数据
-            !this.loading &&
-            // 只有往前滚动我们才触发
-            scrollDirection === 'forward' &&
-            // 5 = (列表高度 / 单项列表高度)
-            // 100 = 滚动提前加载量，可根据样式情况调整
-            scrollOffset >= (dataLen - 10) * itemSize // 滚动距离 > 90*100 = 9000 主要是这里的计算设置
-          ) {
-            console.log("++++++++++++++++")
-            this.listReachBottom()
-          }
-        }}
+        onScrollToLower={listReachBottom}
       />
     )
-  }
-}
+};
+
+export default Index;
